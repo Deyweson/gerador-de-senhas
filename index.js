ff6110748b49bb82731a5e2bea9f0d762d36ecf2
deyweson <devdeyve@gmail.com>
2023-05-15 22:36:41 -0300

    Adição da escolha de tamanho da senha

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let caracteres = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','x','z','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','W','Z','0','1','2','3','4','5','6','7','8','9'];
+
 let senha = '';
+
 
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
-for (let i = 0; i < 10; i++) {
-    let letra = caracteres[getRandomInt(78)];
-    senha = senha + letra
-}
+ function createNewPassword(){
 
-console.log(senha);
+    let tamanho = document.getElementById("len");
+    let tamanho1 =  parseInt(tamanho.value);
+
+    senha = '';
+    for (let i = 0; i < tamanho1; i++) {
+        let letra = caracteres[getRandomInt(78)];
+        senha = senha + letra;
+    }
+
+    document.querySelector("#password").innerHTML = `Senha: ${senha}`;
+ }
