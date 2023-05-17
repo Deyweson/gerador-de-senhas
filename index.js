295424a6ed1f4ca19a4e23dd165857e662124177
deyweson <devdeyve@gmail.com>
2023-05-16 21:40:49 -0300

    Finalização do projeto

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 let caracteres = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','x','z','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','W','Z','0','1','2','3','4','5','6','7','8','9'];
 
+let apenasNumeros = ['0','1','2','3','4','5','6','7','8','9'];
+
+let apenasLetras = ['a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','x','z','A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','W','Z'];
+
 let senha = '';
 
-
-function getRandomInt(max){
+function random(max){
     return Math.floor(Math.random() * max);
 }
 
- function createNewPassword(){
+ function novaSenha(){
 
-    let tamanho = document.getElementById("len");
-    let tamanho1 =  parseInt(tamanho.value);
+    let Tamanho = document.getElementById("len");
+    let tamanhoSenha =  parseInt(Tamanho.value);
+    let senhaOpcao = document.getElementById("tipoSenha").value;
 
     senha = '';
-    for (let i = 0; i < tamanho1; i++) {
-        let letra = caracteres[getRandomInt(78)];
-        senha = senha + letra;
+    
+    if (senhaOpcao === 'letras'){
+        for (let i = 0; i < tamanhoSenha; i++) {
+            let letra = apenasLetras[random(48)];
+            senha = senha + letra;
+        }
+    } else if (senhaOpcao == 'num'){
+        for (let i = 0; i < tamanhoSenha; i++) {
+            let letra = apenasNumeros[random(10)];
+            senha = senha + letra;
+        }
+    } else if('caracteres') {
+        for (let i = 0; i < tamanhoSenha; i++) {
+            let letra = caracteres[random(78)];
+            senha = senha + letra;
+        }
     }
 
-    document.querySelector("#password").innerHTML = `Senha: ${senha}`;
+    if (tamanhoSenha > 20){
+        document.querySelector("#senha").innerHTML = ``;
+    } else {
+        document.querySelector("#senha").innerHTML = `${senha}`;
+    }
+
  }
+ 
